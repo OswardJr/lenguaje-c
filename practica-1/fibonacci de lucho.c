d9a3be198d250c12a13383b9a73b2f0c66ff1a4c
LuchoHn <eleazar.hernandez27@gmail.com>
2015-05-26 13:09:45 -0430

    Update fibonacci de lucho.c

--- practica-1/fibonacci de lucho.c
+++ practica-1/fibonacci de lucho.c
@@ -6,13 +6,18 @@ int num,ant1=0,ant2=1,fibo=0;
     printf("introduzca el numero que quiere saber si es fibonacci: ");
         scanf("%d",&num);
 
-    while (fibo<=num){
+    while (fibo<num){
 
         fibo=ant1 + ant2;
         ant1=ant2;
         ant2=fibo;
         printf("%d ,",fibo);
-       }
-       
-    return 0;
+    }
+
+        if (fibo==num)
+            printf("%d",fibo);
+
+            else
+                printf("%d",fibo);
+return 0;
 }
