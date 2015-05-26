7868609d8aa9d8ef81a5f82418ad7080dc109fb3
LuchoHn <eleazar.hernandez27@gmail.com>
2015-05-26 12:29:02 -0430

    Update fibonacci de lucho.c

--- practica-1/fibonacci de lucho.c
+++ practica-1/fibonacci de lucho.c
@@ -6,20 +6,13 @@ int num,ant1=0,ant2=1,fibo=0;
     printf("introduzca el numero que quiere saber si es fibonacci: ");
         scanf("%d",&num);
 
-    do{
+    while (fibo<=num){
 
         fibo=ant1 + ant2;
         ant1=ant2;
         ant2=fibo;
-        
+        printf("%d ,",fibo);
+       }
        
-       }while(fibo<num);
-        
-        if (num==fibo)
-            printf("pertenece a la serie");
-
-            else
-            printf("No pertenece a la serie");
-
     return 0;
 }
