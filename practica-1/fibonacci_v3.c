f629df26851fe4237edd09904995461cac77d705
Yonathan Moncada <ye_heme@hotmail.com>
2015-05-26 17:54:21 -0430

    Resuelto por Yonathan.

--- practica-1/fibonacci_v3.c
+++ practica-1/fibonacci_v3.c
@@ -3,12 +3,12 @@ int main(){
     int n, ant1=0, ant2=1, fib=0;
     printf("Introduzca el número que desea ubicar en la serie Fibonacci: ");
     scanf("%d",&n);
+    printf("1 ");
     do{
-        fib = ant1 + ant2;
-        ant1 = ant2;
-        ant2 = fib;
-        printf("%d ",fib);
-    }while(fib<n);
-    fib = ant1 + ant2;
+            fib = ant1 + ant2;
+            ant1 = ant2;
+            ant2 = fib;
+            printf("%d ",fib);
+    }while(n>fib);
     return 0;
 }
