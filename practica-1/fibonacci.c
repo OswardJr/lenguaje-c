a52c0193e5657824acbd450dc3d7ba6f32b5d621
Yonathan Moncada <ye_heme@hotmail.com>
2015-05-26 17:31:53 -0430

    Resuelto por Jhonny.

--- practica-1/fibonacci.c
+++ practica-1/fibonacci.c
@@ -3,17 +3,17 @@ int main(){
 	int suma=0,a=0,b=1, x;
 	printf ("introduzca un numero: ");
 	scanf("%d",&x);
-		while(suma<=x){
-			suma=b+a;
-			a = b;
-			b = suma;
-			if (suma==x){
-				printf("%d ,",suma);
-			}
-			else if (suma!=x){
-				printf("%d ,",suma);
-			}	
-		suma++;
+	while(suma<=x){
+		suma=b+a;
+		a = b;
+		b = suma;
+		if (suma==x){
+			printf("%d ,",suma);
+		}
+		else if (suma!=x){
+			printf("%d ,",suma);
 		}	
-		return 0;
+		suma++;
+	}	
+	return 0;
 }
